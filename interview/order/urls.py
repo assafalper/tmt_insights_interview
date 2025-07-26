b55d5349017c1eb3230a870f74b9ee3724b69c1e
assafalper <alperassaf@gmail.com>
2025-07-25 21:02:04 -0700

    Write an endpoint that lists orders that are between a particular start and embargo date.

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, OrderListbetweenEmbargoDates
 
 
 urlpatterns = [
     path("tags/", OrderTagListCreateView.as_view(), name="order-detail"),
     path("", OrderListCreateView.as_view(), name="order-list"),
+    path("embargo_date", OrderListbetweenEmbargoDates.as_view(), name="embargo_date-list"),
 ]
--- interview/order/views.py
+++ interview/order/views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render
+from django.db.models import F, Q
+from django.utils.dateparse import parse_datetime
+
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.request import Request
+
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
 
-# Create your views here.
+
+class OrderListbetweenEmbargoDates(generics.ListCreateAPIView):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return Order.objects.all()
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        date_str = request.GET.get("date")
+        if not date_str:
+            queryset = self.get_queryset()
+            serializer = self.serializer_class(queryset, many=True)
+            return Response(serializer.data, status=200)
+
+        parsed_date = parse_datetime(date_str)
+        if parsed_date is None:
+            return Response("Invalid date format. Use ISO 8601.", status=400)
+
+        filtered_qs = self.get_queryset().filter(
+            Q(start_date__lte=parsed_date) & Q(embargo_date__gte=parsed_date)
+        )
+
+        serializer = self.serializer_class(filtered_qs, many=True)
+        return Response(serializer.data, status=200)
+
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
